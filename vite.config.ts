93f3915e7b0bc21cad6ff895b671786bb4e0bd7b
serge101pro <serge101.pro@gmail.com>
2026-01-31 02:13:51 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
-// vite.config.ts
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'lucide-react'],
-          ui: ['@/components/ui/button', '@/components/ui/dialog'], // и другие UI компоненты
-        },
-      },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    chunkSizeWarningLimit: 600,
   },
-});
+})
